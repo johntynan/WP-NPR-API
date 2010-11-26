b1a7660f9415583acae5c16cf18486482c6f002c
John Tynan <johntynan@titans.dreamhost.com>
2010-11-26 13:40:34 -0800

    fixed typo in sendvariables.js, moved where story_id is found into if statement

--- sendvariables.js
+++ sendvariables.js
@@ -3,16 +3,16 @@ story_hostname = window.location.hostname
 story_url = window.location
 story_title = document.title
 story_title = escape(story_title);
-story_id = NPR.community.storyId
 
 // alert(story_hostname);
 // alert(story_url);
 // alert(story_title);
-// alert(story_id);
 
-if (story_hostname =! 'www.npr.org') {
-        alert("This bookmarklet is intended to be used in conjunction with content on NPR.org");
+if (story_hostname != 'www.npr.org') {
+    alert("This bookmarklet is intended to be used in conjunction with content on NPR.org");
     } else {
-        location.replace('http://johntynan.com/wp-admin/edit.php?page=get-npr-stories&nprid=' + story_id );
+    story_id = NPR.community.storyId
+    // alert(story_id);
+    location.replace('http://johntynan.com/wp-admin/edit.php?page=get-npr-stories&nprid=' + story_id );
 }
 
